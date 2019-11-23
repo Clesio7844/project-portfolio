ba1ccc0ee1952a835d81d7d2904a41617855138d
Cleaio7844 <daviddecastro41@yahoo.co.uk>
2019-11-23 16:19:59 +0000

    first commit

---
+++ react-portfolio1/src/navigation.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class Navigation extends Component{
+    render(){
+        return(
+            <nav>
+
+
+            </nav>  
+
+        );
+    }
+}
+
+export default App;
